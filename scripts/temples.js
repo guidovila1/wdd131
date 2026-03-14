7f01a116e14fcea9a8da38df52dbd9f22437de28
guidovila1 <guidovilamontao@byupathways.edu>
2026-03-14 09:38:22 -0600

    W02 Assigment

--- scripts/temples.js
+++ scripts/temples.js
@@ -1,3 +1,15 @@
+const hamButton = document.querySelector('#menu');
+const navigation = document.querySelector('.navigation');
+const header = document.querySelector("header");
+
+hamButton.addEventListener('click', () => {
+    navigation.classList.toggle('open');
+    hamButton.classList.toggle('open');
+    header.classList.toggle('menu-open');
+});
+
+
+
 const today = new Date();
 document.getElementById("currentyear").innerHTML = today.getFullYear();
 document.getElementById("lastModified").innerHTML = "Last Modified: " + document.lastModified;
